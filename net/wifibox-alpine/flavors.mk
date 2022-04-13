f1f9f9946376e50bf5c17cfc880488c592b70595
PÁLI Gábor János <pali.gabor@gmail.com>
2022-04-13 03:24:45 +0200

    net/wifibox-alpine: Use linux-firmware tarball directly.

--- net/wifibox-alpine/flavors.mk
+++ net/wifibox-alpine/flavors.mk
@@ -1,4 +1,3 @@
-_FLAVORS=	ath6k ath9k ath10k ath11k b43legacy b43 brcm\
-		mwlwifi other rtlwifi rtw88 rtw89
+_FLAVORS=	ath6k ath9k ath10k ath11k brcm iwlwifi mwlwifi rtlwifi rtw88 rtw89
 FLAVORS=	default ${_FLAVORS}
 FLAVOR?=	default
